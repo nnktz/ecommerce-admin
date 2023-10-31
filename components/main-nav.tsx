4921624a91e4beb2127d4e77b8bef2551bb044f5
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-31 17:53:27 +0700

    Settings page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -14,14 +14,21 @@ export const MainNav = ({
 
   const routes = [
     {
-      href: `${params.storedId}/settings`,
+      href: `/${params.storeId}`,
+      label: 'Overview',
+      active: pathname === `/${params.storeId}`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
       label: 'Settings',
-      active: pathname === `${params.storedId}/settings`,
+      active: pathname === `/${params.storeId}/settings`,
     },
   ];
 
   return (
-    <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)}>
+    <nav
+      className={cn('flex items-center space-x-4 lg:space-x-6', className)}
+      {...props}>
       {routes.map((route) => (
         <Link
           key={route.href}
