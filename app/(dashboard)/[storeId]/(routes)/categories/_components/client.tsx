3e2faea0bed687fc8c08a605ab6463ef93605b52
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-11-04 15:24:45 +0700

    Categories entity

--- app/(dashboard)/[storeId]/(routes)/categories/_components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/_components/client.tsx
@@ -8,13 +8,13 @@ import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import { DataTable } from '@/components/ui/data-table';
 import { ApiList } from '@/components/ui/api-list';
-import { BillboardColumn, columns } from './columns';
+import { CategoryColumn, columns } from './columns';
 
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface CategoryClientProps {
+  data: CategoryColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
@@ -22,12 +22,12 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
     <>
       <div className='flex items-center justify-between'>
         <Heading
-          title={`Billboards (${data.length})`}
-          description='Manage billboards for your store'
+          title={`Categories (${data.length})`}
+          description='Manage categories for your store'
         />
 
         <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}>
           <Plus className='mr-2 h-4 w-4' />
           Add New
         </Button>
@@ -43,14 +43,14 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
 
       <Heading
         title='API'
-        description='API calls for Billboards'
+        description='API calls for Categories'
       />
 
       <Separator />
 
       <ApiList
-        entityName='billboards'
-        entityIdName='billboardId'
+        entityName='categories'
+        entityIdName='categoryId'
       />
     </>
   );
